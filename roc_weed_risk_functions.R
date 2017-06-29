a7bb102b71ebba1c4ab65a67a7f3c0a5f82305ad
bparment1 <benoit.parmentier@gmail.com>
2017-06-29 14:30:00 -0400

    running glm for each random sampling and saving ROC plot and data

--- roc_weed_risk_functions.R
+++ roc_weed_risk_functions.R
@@ -44,55 +44,113 @@ library(gplots)
 
 #
 
-run_model_fun <- function(data_df,model_formula_str,model_opt,data_testing=NULL,save_fig=F,out_dir=".",out_suffix=""){
+run_model_fun <- function(data_df,model_formula_str,model_opt,data_testing=NULL,num_cores=1,out_dir=".",out_suffix=""){
   #data_df: input data.frame with data used in modeling
   #model_formula_str
   #model_opt: "logistic","randomForest"
   #out_dir
   #out_suffix
   
-  if(model_opt=="logistic"){
-    if(class(data_df)!="list"){
-      mod <- glm(model_formula_str,data = data_df) #this is the training data!!
-    }else
-      mclapply(,
-               )
-    
+  ### Make a list if data is not a list:
+  if(class(data_df)!="list"){
+    data_df <- list(data_df)
   }
   
-  ### add randomForest option
+  if(!is.null(data_testing)){
+    if(class(data_testing)!="list"){
+      data_testing <- list(data_testing)
+    }
+  }
+
+  if(model_opt=="logistic"){
+    #
+    #if(class(data_df)!="list"){
+    #  list_mod <- glm(model_formula_str,data = data_df,family=binomial()) #this is the training data!!
+    #}else{
+      
+    list_mod <- mclapply(1:length(data_df),
+                           FUN=function(i,model_formula_str,data_df){data_input<-data_df[[i]]; 
+                           mod <-  glm(model_formula_str, data = data_input);
+                           return(mod)},
+                           model_formula_str=model_formula_str,
+                           data_df=data_df,
+                           mc.preschedule = FALSE,
+                           mc.cores =num_cores)
+      
+    ### Apprend prediction to training data.frame!!!
+    
+    if(!is.null(data_testing)){
+      #
+      list_predicted_val <- mclapply(1:length(data_df),
+                                     FUN=function(i,list_mod,data_testing){data_v<-data_testing[[i]]; 
+                                     mod <- list_mod[[i]];
+                                     predicted_val <- predict(mod,newdata=data_v,type='response');
+                                     return(predicted_val)},
+                                     list_mod=list_mod,
+                                     data_testing=data_testing,
+                                     mc.preschedule = FALSE,
+                                     mc.cores =num_cores)
+      
+    }else{
+      list_predicted_val <- NULL
+    }
+      
+  }
   
-  mod$fitted.values #these are the probability values from ROC
-  data_df[,y_var]
-  table(data_df[,y_var])
+  ##if(model=="RF)##
   
+  model_obj <- list(list_mod,list_predicted_val,data_df,data_testing)
+  names(model_obj) <- c("mod","predicted_val","data_training","data_testing")
+  return(model_obj)
+} 
+  
+  
+ROC_evaluation_fun <- function(i,list_data,y_var,predicted_val,save_fig=T,out_suffix="",out_dir="."){
+
+  ### Now do accuracy assessment:
+
+  data_df <- list_data[[i]]
+  dataset_name <- names(list_data)[i]
+  #mod$fitted.values #these are the probability values from ROC
+  #data_df[,y_var]
+  #table(data_df[,y_var])
+
+  mask_val <- 1:length(predicted_val[[i]])
+  rocd2 <- ROC(index=predicted_val[[i]], 
+               boolean=data_df[[y_var]], 
+               mask=mask_val, 
+               nthres = 100)
+  
+  AUC_val <- slot(rocd2,"AUC") #this is your AUC from the logistic modeling
+
+  ROC_table <- slot(rocd2,"table")
+  
+  
+  #Plot ROC curve:
   
   if(save_fig==TRUE){
     
-    out_suffix_str <- paste0("full_data_",out_suffix)
+    out_suffix_str <- paste0(dataset_name,out_suffix)
     
     res_pix<-480 #set as function argument...
     col_mfrow<-1
     #row_mfrow<-2
     row_mfrow<-1
     
-    png_file_name<- paste("Figure_","ROC_plot_",out_suffix_str,".png", sep="")
+    png_filename<- paste("Figure_","ROC_plot_",out_suffix_str,".png", sep="")
     
-    png(filename=file.path(out_dir,png_file_name),
+    png(filename=file.path(out_dir,png_filename),
         width=col_mfrow*res_pix,height=row_mfrow*res_pix)
     par(mfrow=c(row_mfrow,col_mfrow))
     
-    mask_val <- 1:nrow(data_df)
-    rocd2 <- ROC(index=mod$fitted.values, boolean=data_df[[y_var]], mask=mask_val, nthres = 100)
-    
-    slot(rocd2,"AUC") #this is your AUC from the logistic modeling
-    #Plot ROC curve:
-    plot(rocd2)
-    
+    plot(rocd2) #add option later...
     dev.off()
   }
   
-  return(png_file_name)
+  ###### prepare object ####
+  roc_obj <- list(AUC_val,ROC_table,png_filename)
+  names(roc_obj) <-c("AUC_val","ROC_table","png_filename")
+  return(roc_obj)
   
 }
 
